1c7f351983d8fcd79d4d56da6acf37df2245d8a2
kateabr <kateabr@yandex.ru>
2015-09-25 01:51:32 +0400

    #1

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,11 @@
+#include <assert.h>
+#include <cmath>
+
+#define DBL_EPSILON 2.2204460492503131E-16
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+    return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
@@ -166,4 +171,12 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main()
+{
+	assert(abs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON, "test_1.1");
+	assert(abs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON, "test_1.2");
+	assert(abs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON, "test_1.3");
+	assert(abs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON, "test_1.4");
 }
