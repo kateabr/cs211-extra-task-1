6f93697c8ccecc4760e76689cab32e7e241ae04b
kateabr <kateabr@yandex.ru>
2015-09-25 02:02:28 +0400

    #4

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -69,6 +69,10 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+	assert(hours >= 0);
+	int hours_int = hours;
+	double hours_fract = hours - hours_int;
+	return hours_int % 24 + hours_fract;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -191,4 +195,10 @@ int main()
 	assert(abs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON, "test_3.1");
 	assert(abs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON, "test_3.2");
 	assert(abs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON, "test_3.3");
+	
+	assert(abs(to_24_hour_clock(24)) < DBL_EPSILON, "test_4.1");
+	assert(abs(to_24_hour_clock(48)) < DBL_EPSILON, "test_4.2");
+	assert(abs(to_24_hour_clock(25) - 1) < DBL_EPSILON, "test_4.3");
+	assert(abs(to_24_hour_clock(4) - 4) < DBL_EPSILON, "test_4.4");
+	assert(abs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON, "test_4.5");
 }
